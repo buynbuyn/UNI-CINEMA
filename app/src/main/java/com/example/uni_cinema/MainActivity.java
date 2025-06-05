6d05d0580975b6e8f0410067b47d692f18ed0fe8
buynbuyn <lephambaouyen27@gmail.com>
2025-06-05 22:26:36 +0700

    baouyen

---
+++ app/src/main/java/com/example/uni_cinema/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.uni_cinema;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.uni_cinema.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+    private NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+
+        binding.appBarMain.fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .setAnchorView(R.id.fab)
+                .show());
+
+        DrawerLayout drawer = binding.drawerLayout;
+        BottomNavigationView bottomNavigationView = binding.bottomNavigation;
+        NavigationView navigationView = binding.navView;
+
+        // Khởi tạo NavController trước khi sử dụng
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+
+        // Cấu hình AppBar với cả Navigation Drawer & Bottom Navigation
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_search, R.id.nav_profile)
+                .setOpenableLayout(drawer)
+                .build();
+
+        // Thiết lập liên kết với Navigation Component
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
+    }
+}
